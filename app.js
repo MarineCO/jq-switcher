cbf0e8c848e460257dc8387c29d59926e7dba962
marineco <marinecolonge@gmail.com>
2016-10-07 09:37:09 +0200

    Exercice jq-switcher check

--- app.js
+++ app.js
@@ -5,22 +5,41 @@ var species = {
 };
 
 function main(){
+	
+
 	/* 
 	tout votre code doit se trouver dans cette fonction,
 	vous pouvez biensur créer d'autres fonctions si nécessaire
 	*/
 
 	// 1. placez un listener sur le click des éléments <button>
+
+	$("button").click(function(){
+
 	// 2. dans le callback récupérez le data attribute animal
+
+	var animal = $(this).data('animal');
+		console.log(animal)
+
 	// 3. récupérerez dans l'objet species la valeur correspond à l'attribut récupéré
+
+	var valeur = species[animal];
+		console.log(valeur);
+
+
 	// 4. ajoutez élément img en assignant à son attibut src la valeur précédemment récupérée
+	$("img").attr('src', 'img/' + valeur);
+
+
+	});
 	// 5. inserez cet élément dans l'élément ayant l'id holder
+	// cf html
+
 	// 
-	// Conseils : avancez étape par étape, console.log et debugger autant que nécessaires !
-	
-	
-	 
+	// Conseils : avancez étape par étape, console.log et debugger autant que nécessaires ! 1
+
 }
+
 
 $(document).ready(function(){
 	main();
